9f1349efc9355f01628fca4504a974e2b9f9e5f1
cococollla <cococolla537@gmail.com>
2023-12-25 22:03:05 +0300

    Make array summ in calculate

--- Тема№2 с тестами/Task2/Calculator.js
+++ Тема№2 с тестами/Task2/Calculator.js
@@ -3,20 +3,21 @@ function Calculator(x) {
         throw new Error("Калькулятор вызван без ключевого слова 'new'!");
     }
 
-    this.stack = [{ operator: null, operand: x }];
+    this.stack = [x];
 
     this.plus = function (num) {
-        this.stack.push({ operator: '+', operand: num });
+        this.stack.push(num);
         return this;
     };
 
     this.minus = function (num) {
-        this.stack.push({ operator: '-', operand: num });
+        this.stack.push(-num);
         return this;
     };
 
     this.multiply = function (num) {
-        this.stack.push({ operator: '*', operand: num });
+        let lastNumber = this.stack.pop();
+        this.stack.push(lastNumber * num);
         return this;
     };
 
@@ -24,38 +25,13 @@ function Calculator(x) {
         if (num === 0) {
             throw new Error('Cannot divide by zero');
         }
-        this.stack.push({ operator: '/', operand: num });
+
+        let lastNumber = this.stack.pop();
+        this.stack.push(lastNumber / num);
         return this;
     };
 
     this.calculate = function () {
-        let tempStack = [];
-        let current = this.stack[0];
-
-        for (let i = 1; i < this.stack.length; i++) {
-            const next = this.stack[i];
-
-            if (next.operator === '*' || next.operator === '/') {
-                current.operand = (next.operator === '*') ?
-                    current.operand * next.operand :
-                    current.operand / next.operand;
-            } else {
-                tempStack.push(current);
-                current = next;
-            }
-        }
-
-        tempStack.push(current);
-        this.stack = tempStack;
-
-        let result = this.stack[0].operand;
-        for (let i = 1; i < this.stack.length; i++) {
-            const next = this.stack[i];
-            result = (next.operator === '+') ?
-                result + next.operand :
-                result - next.operand;
-        }
-
-        return result;
+        return this.stack.reduce((acc, val) => acc + val, 0);
     };
 }
