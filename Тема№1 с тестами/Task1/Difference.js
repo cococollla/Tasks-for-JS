4161a5518e6958dfd01b913f77830911ca246fab
nsbel <nsbelyaev@outlook.com>
2023-12-25 15:41:44 +0300

    Fix function MyDifference

--- Тема№1 с тестами/Task1/Difference.js
+++ Тема№1 с тестами/Task1/Difference.js
@@ -1,3 +1,4 @@
-function MyDifference(arrA, arrB) {
-    return Array.from(arrA.filter(x => !arrB.includes(x)));
+function MyDifference(arrA, ...arrB) {
+    const set = new Set(arrB.flat());
+    return Array.from(arrA.filter(element => !set.has(element)));
 }
--- Тема№1 с тестами/Task1/IsEqual.js
+++ Тема№1 с тестами/Task1/IsEqual.js
@@ -16,6 +16,10 @@ function MyIsEqual(value, other) {
     }
 
     if (isObject(value) && isObject(other)) {
+        if (value.constructor !== other.constructor) {
+            return false;
+        }
+
         if (value instanceof Date && other instanceof Date) {
             if (JSON.stringify(value) === JSON.stringify(other)) {
                 return true;
@@ -24,19 +28,15 @@ function MyIsEqual(value, other) {
         }
 
         if (value instanceof Map && other instanceof Map) {
-            return compareMaps(value, other)
+            return MyIsEqual(Array.from(value), Array.from(other))
         }
 
         if (value instanceof Set && other instanceof Set) {
-            return compareSet(value, other)
+            return MyIsEqual(Array.from(value),Array.from(other))
         }
 
         if (value instanceof RegExp && other instanceof RegExp) {
             return value.source === other.source && value.flags === other.flags;
-        }
-
-        if (value.constructor !== other.constructor) {
-            return false;
         }
 
         let valueKeys = Object.keys(value);
@@ -65,29 +65,4 @@ function isObject(object) {
     }
 
     return false;
-}
-
-function compareMaps(map1, map2) {
-    let testVal;
-    if (map1.size !== map2.size) {
-        return false;
-    }
-    for (let [key, val] of map1) {
-        testVal = map2.get(key);
-        if (testVal !== val || (testVal === undefined && !map2.has(key))) {
-            return false;
-        }
-    }
-    return true;
-}
-
-function compareSet(set1, set2) {
-    if (set1.size !== set2.size) {
-        return false;
-    }
-
-    let arrForSet1 = Array.from(set1);
-    let arrForSet2 = Array.from(set2);
-
-    return MyIsEqual(arrForSet1, arrForSet2);
 }
